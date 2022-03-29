11d1743876698da36a528d85933c03456e087c16
edward2100 <59863104+edward2100@users.noreply.github.com>
2022-03-29 18:40:49 +0800

    Create Simulation-with-Simpy.py

---
+++ Simulation-with-Simpy.py
@@ -0,0 +1,8 @@
+# Set up the environment
+env = simpy.Environment()
+
+# Assume you've defined checkpoint_run() beforehand
+env.process(checkpoint_run(env, num_booths, check_time, passenger_arrival))
+
+# Let's go!
+env.run(until=10)
